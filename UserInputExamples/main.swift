70a8de5dd5f44be0a311ab10d838b12db0c4276a
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-02-02 09:34:00 -0500

    done user input example to understand comparison of values

--- UserInputExamples/main.swift
+++ UserInputExamples/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+// Simulate user input
+let givenInput = "50"
 
+// Simulate target number to guess
+let target = 37
+
+// Attempt to convert the String input to an Int
+
+guard let selectedNumber = Int(givenInput) else {
+    print("Please provide an integer.")
+    exit(0)
+}
+
+// Compare input to the target
+if selectedNumber > target {
+    print("Guess lower next time.")
+}
